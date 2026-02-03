77725e8cabe91a8296f3a028b81a38ce52a617ff
Fasamii <fasami0512@gmail.com>
2026-02-04 00:07:32 +0100

    comment marks for js

--- after/queries/javascript/injections.scm
+++ after/queries/javascript/injections.scm
@@ -1,42 +1,9 @@
-;; extends
+; extends
 
-; TODO: Write similar queries for other languages
-
-;; SQL via comment marker
-((comment) @_comment
- (string
-   (string_fragment) @injection.content) 
- (#lua-match? @_comment "sql")
- (#set! injection.language "sql"))
-
-((comment) @_comment
-  (template_string
-    (string_fragment) @injection.content)
-  (#lua-match? @_comment "sql")
-  (#set! injection.language "sql"))
-
-;; LUA via comment marker
-((comment) @_comment
- (string
-   (string_fragment) @injection.content) 
- (#lua-match? @_comment "lua")
- (#set! injection.language "lua"))
-
-((comment) @_comment
-  (template_string
-    (string_fragment) @injection.content)
-  (#lua-match? @_comment "lua")
-  (#set! injection.language "lua"))
-
-;; GraphQL via comment marker
-((comment) @_comment
- (string
-   (string_fragment) @injection.content) 
- (#lua-match? @_comment "graphql")
- (#set! injection.language "graphql"))
-
-((comment) @_comment
-  (template_string
-    (string_fragment) @injection.content)
-  (#lua-match? @_comment "graphql")
-  (#set! injection.language "graphql"))
+; comment marker in declarations and foos (any)
+(
+  (comment) @injection.language
+  . (_ (string_fragment) @injection.content)
+  (#set! injection.combined)
+  (#gsub! @injection.language "^%s*/%*%s*(.-)%s*%*/%s*$" "%1")
+)
